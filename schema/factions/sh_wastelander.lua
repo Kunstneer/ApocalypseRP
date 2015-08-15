3812e0956774390a734f410c5446a970dd8d34ea
Justin <NDKilla@users.noreply.github.com>
2015-08-15 17:37:03 +0200

    Create sh_wastelander.lua

---
+++ schema/factions/sh_wastelander.lua
@@ -0,0 +1,8 @@
+FACTION.name = "Wastelander"
+FACTION.desc = "A desert wastelander."
+FACTION.color = Color(25, 180, 30)
+FACTION.isDefault = true
+FACTION.isGloballyRecognized = false
+FACTION.pay = 0
+
+FACTION_WASTELANDER = FACTION.index
